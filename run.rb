219e69c8d9449253ca0d74ccf3d179675f07e0a0
Erin Turley <erinturley@live.com>
2016-10-10 15:01:50 -0400

    completed working with arrays

--- run.rb
+++ run.rb
@@ -9,13 +9,25 @@ end
 
 get '/years' do
 
-
-
   # code!
+  @yearText = {
+  	:content => "<p>I was born in April of 1996 and have been alive for 20 years.</p>"
+  }
+  @years = (1996..2016).to_a
+
   erb :years, layout: :main
 end
 
 get '/states' do
   # code!
+  #@stateText {
+  #	:content => "<p>I have visited the following states: </p>"
+  #}
+  @stateText = {
+  	:content =>"<p>I have visited the following states:</p>"
+  }
+  @states = ["Washington", "California", "Oregon", "Indiana", "New Jersey", "Arizona"]
+  @states.sort
+  @states.sort! #permanent sort
   erb :states, layout: :main
 end
